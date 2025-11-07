c3166c06a3f0e5e6420ba8f12acca715e2e0ac2d
Carlie Bosier <carlie.bosier@bison.howard.edu>
2025-11-07 12:14:34 -0500

    upload working code

---
+++ boggle_backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import get_game, get_games, create_game, save_game
+
+urlpatterns = [
+    path('game/<int:pk>', get_game, name='get_game'),
+    path('games/', get_games, name='get_games'),
+    path('game/create/<int:size>', create_game, name='create_game'),
+    path('game/save/', save_game, name='save_game')
+]
